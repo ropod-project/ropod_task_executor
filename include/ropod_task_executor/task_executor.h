e829c62737582a5bfa01c3566de0a672e1b605a2
Santosh Thoduka <santosh.thoduka@gmail.com>
2018-07-18 13:50:19 +0200

    updates for processing feedback and sending elevator requests

--- include/ropod_task_executor/task_executor.h
+++ include/ropod_task_executor/task_executor.h
@@ -3,6 +3,11 @@
 
 #include <ros/ros.h>
 #include <ropod_ros_msgs/Task.h>
+#include <ropod_ros_msgs/TaskProgressGOTO.h>
+#include <ropod_ros_msgs/Action.h>
+#include <ropod_ros_msgs/Waypoint.h>
+#include <ropod_ros_msgs/ElevatorRequest.h>
+#include <ropod_ros_msgs/ElevatorRequestReply.h>
 #include <ropod_ros_msgs/ropod_demo_plan.h>
 
 /**
@@ -15,12 +20,55 @@
 class TaskExecutor
 {
 private:
+    /**
+     * types of Actions
+     */
+    enum ActionType
+    {
+        GOTO,
+        DOCK,
+        UNDOCK,
+        REQUEST_ELEVATOR,
+        GOTO_ELEVATOR,
+        ENTER_ELEVATOR,
+        EXIT_ELEVATOR
+    };
+
+    /**
+     * internal state machine states
+     */
+    enum State
+    {
+        INIT,
+        DISPATCHING_ACTION,
+        EXECUTING_ACTION,
+        TASK_DONE
+    };
+
+    /**
+     * Current state in internal state machine
+     */
+    State state;
+
+    /**
+     * ros private node handle
+     */
     ros::NodeHandle nh;
 
     /**
      * Subscriber for task message
      */
     ros::Subscriber task_sub;
+
+    /**
+     * Subscriber for elevator reply to request
+     */
+    ros::Subscriber elevator_reply_sub;
+
+    /**
+     * Subscriber for TaskProgressGOTO messages (from navigation)
+     */
+    ros::Subscriber task_progress_goto_sub;
 
     /**
      * Publisher for task and action feedback
@@ -43,9 +91,29 @@ private:
     ros::Publisher action_undock_pub;
 
     /**
+     * Publisher for sending elevator requests
+     */
+    ros::Publisher elevator_request_pub;
+
+    /**
+     * Publisher for sending TaskProgressGOTO messages (to com mediator)
+     */
+    ros::Publisher task_progress_goto_pub;
+
+    /**
+     * Reply to elevator request
+     */
+    ropod_ros_msgs::ElevatorRequestReply::Ptr elevator_reply;
+
+    /**
      * Currently executing task
      */
     ropod_ros_msgs::Task::Ptr current_task;
+
+    /**
+     * Type of currently executing action
+     */
+    ActionType current_action_type;
 
     /**
      * Flag indicating if a task has been received
@@ -53,13 +121,8 @@ private:
     bool received_task;
 
     /**
-     * Flag indicating whether task execution is ongoing
-     */
-    bool execution_ongoing;
-
-    /**
      * Flag indicating whether an action within the task is being
-     * executing currently
+     * executed currently
      */
     bool action_ongoing;
 
@@ -68,14 +131,39 @@ private:
      */
     int current_action_index;
 
-public:
-    TaskExecutor();
-    virtual ~TaskExecutor();
+    /**
+     * ID of current action being executed
+     */
+    std::string current_action_id;
+
+    /**
+     * callback for reply to elevator request
+     */
+    void elevatorReplyCallback(const ropod_ros_msgs::ElevatorRequestReply::Ptr &msg);
 
     /**
      * Subscriber callback for task messages
      */
     void taskCallback(const ropod_ros_msgs::Task::Ptr &msg);
+
+    /**
+     * Subscriber callback for task progress messages for GOTO actions
+     */
+    void taskProgressGOTOCallback(const ropod_ros_msgs::TaskProgressGOTO::Ptr &msg);
+
+
+    /**
+     * Publish a message to request an elevator
+     *
+     * @param action message of type "REQUEST_ELEVATOR"
+     * @param task_id UUID of current task
+     * @param cart_type Type of load being carried at the moment
+     */
+    void requestElevator(const ropod_ros_msgs::Action &action, const std::string &task_id, const std::string &cart_type);
+
+public:
+    TaskExecutor();
+    virtual ~TaskExecutor();
 
     /**
      * Main loop; dispatches actions one by one
--- src/task_executor.cpp
+++ src/task_executor.cpp
@@ -1,83 +1,174 @@
 #include <ropod_task_executor/task_executor.h>
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc
 
 TaskExecutor::TaskExecutor() :
+    state(INIT),
     nh("~"),
     received_task(false),
-    execution_ongoing(false),
     action_ongoing(false),
     current_action_index(-1)
 {
     task_sub = nh.subscribe("task", 1, &TaskExecutor::taskCallback, this);
     task_feedback_pub = nh.advertise<ropod_ros_msgs::Task>("feedback", 1);
-    action_goto_pub = nh.advertise<ropod_ros_msgs::Action>("go_to", 1);
-    action_dock_pub = nh.advertise<ropod_ros_msgs::Action>("dock", 1);
-    action_undock_pub = nh.advertise<ropod_ros_msgs::Action>("undock", 1);
+    action_goto_pub = nh.advertise<ropod_ros_msgs::Action>("GOTO", 1);
+    action_dock_pub = nh.advertise<ropod_ros_msgs::Action>("DOCK", 1);
+    action_undock_pub = nh.advertise<ropod_ros_msgs::Action>("UNDOCK", 1);
+
+    elevator_reply_sub = nh.subscribe("elevator_reply", 1, &TaskExecutor::elevatorReplyCallback, this);
+    elevator_request_pub = nh.advertise<ropod_ros_msgs::ElevatorRequest>("elevator_request", 1);
+
+    task_progress_goto_pub = nh.advertise<ropod_ros_msgs::TaskProgressGOTO>("progress_goto_out", 1);
+    task_progress_goto_sub = nh.subscribe("progress_goto_in", 1, &TaskExecutor::taskProgressGOTOCallback, this);
 }
 
 TaskExecutor::~TaskExecutor()
 {
 }
 
+void TaskExecutor::run()
+{
+    if (state == INIT)
+    {
+        if (received_task)
+        {
+            state = DISPATCHING_ACTION;
+            current_action_index = 0;
+            current_action_id = "";
+        }
+        return;
+    }
+    else if (state == DISPATCHING_ACTION)
+    {
+        if (current_action_index >= current_task->robot_actions.size())
+        {
+            state = TASK_DONE;
+            return;
+        }
+        current_action_id = current_task->robot_actions[current_action_index].action_id;
+        action_ongoing = true;
+        ropod_ros_msgs::Action action = current_task->robot_actions[current_action_index];
+        if (action.type == "GOTO")
+        {
+            action_goto_pub.publish(action);
+            current_action_type = GOTO;
+        }
+        else if (action.type == "DOCK")
+        {
+            action_dock_pub.publish(action);
+            current_action_type = DOCK;
+        }
+        else if (action.type == "UNDOCK")
+        {
+            action_undock_pub.publish(action);
+            current_action_type = UNDOCK;
+        }
+        else if (action.type == "REQUEST_ELEVATOR")
+        {
+            current_action_type = REQUEST_ELEVATOR;
+            elevator_reply = nullptr;
+            requestElevator(action, current_task->task_id, current_task->cart_type);
+        }
+        else if (action.type == "ENTER_ELEVATOR")
+        {
+            current_action_type = ENTER_ELEVATOR;
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Got invalid action: " << action.type);
+            ROS_ERROR_STREAM("Stopping task execution");
+            action_ongoing = false;
+            state = INIT;
+            return;
+        }
+        state = EXECUTING_ACTION;
+        return;
+    }
+    else if (state == EXECUTING_ACTION)
+    {
+        if (!action_ongoing)
+        {
+            current_action_index++;
+            current_action_id = "";
+            state = DISPATCHING_ACTION;
+            return;
+        }
+        if (current_action_type == REQUEST_ELEVATOR)
+        {
+            if (elevator_reply)
+            {
+                std::cout << "Received elevator request reply " << std::endl;
+                // TODO: make sure we get reply from the correct query
+                if (elevator_reply->query_success)
+                {
+                    std::string wp = elevator_reply->elevator_waypoint;
+                    // send go to elevator_reply->waypoint here
+                    elevator_reply = nullptr;
+                    current_action_type = GOTO_ELEVATOR;
+                    action_ongoing = true;
+                }
+            }
+        }
+        return;
+    }
+    else if (state == TASK_DONE)
+    {
+        received_task = false;
+        current_action_index = -1;
+        current_action_id = "";
+        action_ongoing = false;
+        state = INIT;
+        std::cout << "Task done! " << std::endl;
+        return;
+    }
+}
+
+void TaskExecutor::requestElevator(const ropod_ros_msgs::Action &action, const std::string &task_id, const std::string &cart_type)
+{
+    ropod_ros_msgs::ElevatorRequest er;
+    boost::uuids::uuid uuid = boost::uuids::random_generator()();
+    std::stringstream ss;
+    ss << uuid;
+    er.query_id = ss.str();
+    er.command = "CALL_ELEVATOR";
+    er.start_floor = action.start_floor;
+    er.goal_floor = action.goal_floor;
+    er.task_id = task_id;
+    er.load = cart_type;
+    elevator_request_pub.publish(er);
+}
 
 void TaskExecutor::taskCallback(const ropod_ros_msgs::Task::Ptr &msg)
 {
-    if (execution_ongoing)
+    if (state != INIT)
     {
         ROS_WARN_STREAM("Cancelling previous task");
         // TODO: properly cancel an ongoing task
-        execution_ongoing = false;
         action_ongoing = false;
-        current_action_index = -1;
+        current_action_index = 0;
+        current_action_id = "";
+        state = DISPATCHING_ACTION;
     }
     current_task = msg;
     received_task = true;
 }
 
-void TaskExecutor::run()
+void TaskExecutor::elevatorReplyCallback(const ropod_ros_msgs::ElevatorRequestReply::Ptr &msg)
 {
-    if (!received_task)
-    {
-        return;
-    }
-    // if task has been received, is ongoing and the current action is
-    // also ongoing, do nothing
-    if (received_task && execution_ongoing && action_ongoing)
-    {
-        return;
-    }
-    else if (received_task && !execution_ongoing)
-    {
-        current_action_index = 0;
-    }
+    elevator_reply = msg;
+}
 
-    if (current_action_index >= current_task->robot_actions.size())
+void TaskExecutor::taskProgressGOTOCallback(const ropod_ros_msgs::TaskProgressGOTO::Ptr &msg)
+{
+    msg->task_id = current_task->task_id;
+    task_progress_goto_pub.publish(*msg);
+
+    if (msg->status == "reached"  &&
+        msg->sequenceNumber == msg->totalNumber &&
+        msg->action_id == current_action_id)
     {
-        // we're done with the task
-        return;
-    }
-    else
-    {
-        execution_ongoing = true;
-        ropod_ros_msgs::Action action = current_task->robot_actions[current_action_index];
-        if (action.type == "go_to")
-        {
-            action_goto_pub.publish(action);
-            action_ongoing = true;
-        }
-        else if (action.type == "dock")
-        {
-            action_dock_pub.publish(action);
-            action_ongoing = true;
-        }
-        else if (action.type == "undock")
-        {
-            action_dock_pub.publish(action);
-            action_ongoing = true;
-        }
-        else
-        {
-            ROS_ERROR_STREAM("Got invalid action: " << action.type);
-        }
+        action_ongoing = false;
     }
 }
 
